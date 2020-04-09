b087a5eddd32939b05a916c9577e0db4edd6c4f9
James Coombs <coombsj36@gmail.com>
2020-04-09 13:42:36 -0400

    Convert portfolioData to js to better store/retrieve svgs.

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -9,106 +9,18 @@ import { ReactComponent as SqlServerSvg } from "../img/svg/sqlserver.svg";
 import { ReactComponent as SitecoreSvg } from "../img/svg/sitecore.svg";
 
 export default function Skills(props) {
-  function listSkills() {
-    let list = [];
-
-    portfolioData.skills.map((skill) => {
-      const arr = ["redux", "ember", "SQLServer", "SOLR", "Sitecore"];
-
-      if (!arr.includes(Object.keys(skill)[0])) {
-        list.push(
-          <Col key={Object.keys(skill)[0]} sm={3} className="text-center mb-2">
-            <i
-              className={`skill-icon devicon-${Object.keys(skill)[0]}-plain`}
-            ></i>
-            <div>{skill[Object.keys(skill)[0]]}</div>
-          </Col>
-        );
-      } else {
-        if (Object.keys(skill)[0] === "redux") {
-          list.push(
-            <Col
-              key={Object.keys(skill)[0]}
-              sm={3}
-              className="text-center mb-2"
-            >
-              <ReduxSvg
-                style={{
-                  height: "3rem",
-                }}
-              />
-              <div>{skill[Object.keys(skill)[0]]}</div>
-            </Col>
-          );
-        }
-        if (Object.keys(skill)[0] === "ember") {
-          list.push(
-            <Col
-              key={Object.keys(skill)[0]}
-              sm={3}
-              className="text-center mb-2"
-            >
-              <EmberSvg
-                style={{
-                  height: "3rem",
-                }}
-              />
-              <div>{skill[Object.keys(skill)[0]]}</div>
-            </Col>
-          );
-        }
-        if (Object.keys(skill)[0] === "SQLServer") {
-          list.push(
-            <Col
-              key={Object.keys(skill)[0]}
-              sm={3}
-              className="text-center mb-2"
-            >
-              <SqlServerSvg
-                style={{
-                  height: "3rem",
-                }}
-              />
-              <div>{skill[Object.keys(skill)[0]]}</div>
-            </Col>
-          );
-        }
-        if (Object.keys(skill)[0] === "SOLR") {
-          list.push(
-            <Col
-              key={Object.keys(skill)[0]}
-              sm={3}
-              className="text-center mb-2"
-            >
-              <SolrSvg
-                style={{
-                  height: "3rem",
-                }}
-              />
-              <div>{skill[Object.keys(skill)[0]]}</div>
-            </Col>
-          );
-        }
-        if (Object.keys(skill)[0] === "Sitecore") {
-          list.push(
-            <Col
-              key={Object.keys(skill)[0]}
-              sm={3}
-              className="text-center mb-2"
-            >
-              <SitecoreSvg
-                style={{
-                  height: "3rem",
-                }}
-              />
-              <div>{skill[Object.keys(skill)[0]]}</div>
-            </Col>
-          );
-        }
-      }
-    });
-
-    return list;
-  }
-  return <Row className="container skills-row">{listSkills()}</Row>;
+  return (
+    <Row className="container skills-row">
+      {portfolioData.skills.map((skill) => (
+        <Col key={skill.name} sm={3} className="text-center mb-2">
+          {skill.svg ? (
+            React.createElement(skill.svg)
+          ) : (
+            <i className={`skill-icon devicon-${skill.class}-plain`}></i>
+          )}
+          <div>{skill.name}</div>
+        </Col>
+      ))}
+    </Row>
+  );
 }
