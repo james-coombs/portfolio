eb91986357c754cd42c55b58a5ccad5df12b7200
James Coombs <coombsj36@gmail.com>
2020-04-14 14:26:35 -0400

    Updates

---
+++ precache-manifest.0857148cd460a7259158164fc2df9d7c.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c71c51348e294ea895711f2960102bc",
+    "url": "/james-coombs/portfolio/index.html"
+  },
+  {
+    "revision": "2148e164aed733545cc7",
+    "url": "/james-coombs/portfolio/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "11e6b599767fb9c1d5a0",
+    "url": "/james-coombs/portfolio/static/css/main.45a5a084.chunk.css"
+  },
+  {
+    "revision": "2148e164aed733545cc7",
+    "url": "/james-coombs/portfolio/static/js/2.dd5d9237.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/james-coombs/portfolio/static/js/2.dd5d9237.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11e6b599767fb9c1d5a0",
+    "url": "/james-coombs/portfolio/static/js/main.7a5abe2e.chunk.js"
+  },
+  {
+    "revision": "5cae45bd760efcc5ae7a",
+    "url": "/james-coombs/portfolio/static/js/runtime-main.bf019e8e.js"
+  },
+  {
+    "revision": "2f2036cafa4516406b8b67176fb42a41",
+    "url": "/james-coombs/portfolio/static/media/background.2f2036ca.jpg"
+  },
+  {
+    "revision": "75173526cfe25c678967814cee734c6c",
+    "url": "/james-coombs/portfolio/static/media/backlog.75173526.png"
+  },
+  {
+    "revision": "67b23de24b716f1e7c44b86e1374d4a1",
+    "url": "/james-coombs/portfolio/static/media/biologicalsamplescsvapi0.67b23de2.png"
+  },
+  {
+    "revision": "e555ccc9103690c584b78c4b6ca430b2",
+    "url": "/james-coombs/portfolio/static/media/biologicalsamplescsvapi1.e555ccc9.png"
+  },
+  {
+    "revision": "40415e62771bbabf4be7842dd8bf90e6",
+    "url": "/james-coombs/portfolio/static/media/email.40415e62.svg"
+  },
+  {
+    "revision": "b40497e3de69ea73eb5d8fab1dac8c7f",
+    "url": "/james-coombs/portfolio/static/media/ember.b40497e3.svg"
+  },
+  {
+    "revision": "cbc305cccbd2066e275a90f3b54b70fb",
+    "url": "/james-coombs/portfolio/static/media/github.cbc305cc.svg"
+  },
+  {
+    "revision": "e230944c622b8b9765443e25acd98b99",
+    "url": "/james-coombs/portfolio/static/media/headshot.e230944c.jpg"
+  },
+  {
+    "revision": "3b3ed364816aece8aaad46835c479bb4",
+    "url": "/james-coombs/portfolio/static/media/linkedin.3b3ed364.svg"
+  },
+  {
+    "revision": "d71a729aee912748da80a51764cc92a2",
+    "url": "/james-coombs/portfolio/static/media/pdf.d71a729a.svg"
+  },
+  {
+    "revision": "065ddf12dac59752a77c4b524d214546",
+    "url": "/james-coombs/portfolio/static/media/redux.065ddf12.svg"
+  },
+  {
+    "revision": "79b999dc33cb9da374c28438897f6c82",
+    "url": "/james-coombs/portfolio/static/media/sitecore.79b999dc.svg"
+  },
+  {
+    "revision": "da7cda1c220ad97a74364a939230f8db",
+    "url": "/james-coombs/portfolio/static/media/solr.da7cda1c.svg"
+  },
+  {
+    "revision": "1fe11fa0d4f8bbc0493ef2b7f202e775",
+    "url": "/james-coombs/portfolio/static/media/sqlserver.1fe11fa0.svg"
+  },
+  {
+    "revision": "7c233b1f62139be6a2ca6709815910ea",
+    "url": "/james-coombs/portfolio/static/media/sweatersforturtles.7c233b1f.png"
+  },
+  {
+    "revision": "61c04f6e5c6219a0ea58eff9a02b7642",
+    "url": "/james-coombs/portfolio/static/media/tictactoe.61c04f6e.png"
+  },
+  {
+    "revision": "22b86454bfd946c87a7a1b80c76a4c04",
+    "url": "/james-coombs/portfolio/static/media/yamba.22b86454.png"
+  }
+]);
